235a477098b5e8ab9374b6df1c7ca2a20b763b29
1kotik <lyoshakotik@gmail.com>
2026-01-15 17:49:25 +0300

    Implemented questions addon. Questions are displayed on PDP

---
+++ bin/custom/questions/src/by/kotik/questions/populator/ProductQuestionDataPopulator.java
@@ -0,0 +1,47 @@
+package by.kotik.questions.populator;
+
+import by.kotik.questions.model.QuestionModel;
+import de.hybris.platform.commercefacades.product.data.ProductData;
+import de.hybris.platform.commercefacades.user.data.CustomerData;
+import de.hybris.platform.converters.Populator;
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
+import org.springframework.stereotype.Component;
+import questions.data.QuestionData;
+
+import java.util.List;
+
+@Component("productQuestionDataPopulator")
+public class ProductQuestionDataPopulator implements Populator<ProductModel, ProductData> {
+    private final Converter<CustomerModel, CustomerData> customerConverter;
+
+    public ProductQuestionDataPopulator(Converter<CustomerModel, CustomerData> customerConverter) {
+        this.customerConverter = customerConverter;
+    }
+
+    @Override
+    public void populate(final ProductModel productModel, final ProductData productData) throws ConversionException {
+        if (productModel == null || productData == null) {
+            return;
+        }
+        final List<QuestionModel> questionModelList = productModel.getQuestions();
+        final List<QuestionData> questionDataList = questionModelList.stream()
+                .map(this::getQuestionData)
+                .toList();
+        productData.setQuestions(questionDataList);
+    }
+
+    private QuestionData getQuestionData(QuestionModel questionModel) {
+        QuestionData questionData = new QuestionData();
+        questionData.setId(questionModel.getCode());
+        questionData.setQuestion(questionModel.getQuestion());
+        questionData.setAnswer(questionModel.getAnswer());
+        final CustomerData questionCustomer = customerConverter.convert(questionModel.getQuestionCustomer());
+        final CustomerData answerCustomer = customerConverter.convert(questionModel.getAnswerCustomer());
+        questionData.setQuestionCustomer(questionCustomer);
+        questionData.setAnswerCustomer(answerCustomer);
+        return questionData;
+    }
+}
